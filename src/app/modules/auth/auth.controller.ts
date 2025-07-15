4ff3c83261849010f2c8970802d367e774eb0926
kashim rahman mahdi <mahdi.rahman356@gmail.com>
2025-07-15 12:25:16 +0600

    complete authentication useing passport js, handling mongoose and zod error, create interface and model for division and tour

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import httpStatus from "http-status-codes"
 import { catchAsync } from "../../utils/catchAsync"
@@ -9,20 +10,39 @@ import { setAuthCookie } from "../../utils/setCookie"
 import { JwtPayload } from "jsonwebtoken"
 import { createUserToken } from "../../utils/userTokens"
 import { envVars } from "../../config/env"
+import passport from "passport"
 
 
 const credentialsLogin = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
 
-    const loginInfo = await AuthServices.credentialsLogin(req.body)
+    passport.authenticate("local", async (err: any, user: any, info: any) => {
 
-    setAuthCookie(res, loginInfo)
+        if (err) {
+            return next(new AppError(401, err))
+        }
 
-    sendResponse(res, {
-        success: true,
-        statusCode: httpStatus.OK,
-        message: "User Logged In Successfully",
-        data: loginInfo,
-    })
+        if (!user) {
+            return next(new AppError(401, info.message))
+        }
+
+        const userToken = await createUserToken(user)
+
+        const { password: pass, ...rest } = user.toObject()
+
+        setAuthCookie(res, userToken)
+
+        sendResponse(res, {
+            success: true,
+            statusCode: httpStatus.OK,
+            message: "User Logged In Successfully",
+            data: {
+                accessToken: userToken.accessToken,
+                refreshToken: userToken.refreshToken,
+                user: rest
+            }
+        })
+    })(req, res, next)
+
 })
 const getNewAccessToken = catchAsync(async (req: Request, res: Response, next: NextFunction) => {
     const refreshToken = req.cookies.refreshToken
